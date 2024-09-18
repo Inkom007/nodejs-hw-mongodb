27a21e3f2bb485f0c6e4ddbd12cb2772881f2c32
Inkom007 <inkom0703@gmail.com>
2024-09-18 17:54:33 +0100

    update status

--- src/server.js
+++ src/server.js
@@ -41,6 +41,7 @@ export const setupServer = () => {
       return;
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}`,
       data: contact,
     });
