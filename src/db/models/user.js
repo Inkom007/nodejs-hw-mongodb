798220d26511c065f31d6239851651b87933a193
Inkom007 <inkom0703@gmail.com>
2024-10-10 17:17:08 +0100

    etc

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -5,8 +5,6 @@ const usersSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
   },
   { timestamps: true, versionKey: false },
 );
