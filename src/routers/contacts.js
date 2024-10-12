8e96c5bea6af05a0c33dffef4a80b0bc6f0a9331
Inkom007 <inkom0703@gmail.com>
2024-10-12 14:54:55 +0100

    update validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -26,7 +26,7 @@ router.post(
   ctrlWrapper(createContactController),
 );
 router.patch(
-  ':contactId',
+  '/:contactId',
   isValidId,
   validateBody(updateContactSchema),
   ctrlWrapper(patchStudentController),
